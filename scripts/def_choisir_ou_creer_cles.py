3297e6178e21d30f42c001baee21b8f0aad7a008
hyhishem <001data2025@gmail.com>
2025-10-20 20:42:29 +0200

    V1.1 correction CRUD

--- scripts/def_choisir_ou_creer_cles.py
+++ scripts/def_choisir_ou_creer_cles.py
@@ -1,5 +1,5 @@
 ##########################
-#Fonction pour importer ou creer  des clés pour le mode interactif.  Les clé et types sont stockés dans la collection "cles"
+#Fonction gestion des clés: importer, creer, modifier ou supprimer des clés pour le mode interactif.  Les clé et types sont stockés dans la collection "cles"
 ###########################
 from pymongo import MongoClient
 
@@ -9,10 +9,16 @@ def choisir_ou_creer_cles(db, collection):
     
     docs_1 = collection.find({}, {"_id": 0}).limit(1)    # clés présentes dans la collection cible
 
+
     try:
         collection_cles.create_index([("nom_cle")], unique=True, name="index_nom_cle") # index pour imposer l'unicité des clés
     except:
         None
+
+
+############
+# Importer les clés  et types de la collection cible vers la collection cles
+#########
 
     if docs_1:
         choix=input('Voulez vous importer les clés disponible? (oui ou non):').strip()
@@ -20,7 +26,7 @@ def choisir_ou_creer_cles(db, collection):
             for doc in docs_1:
               for nom_cle, valeur in doc.items():
                 try:
-                    collection_cles.insert_one({'nom_cle':nom_cle, 'type_cle':type(valeur).__name__ })  # Importer les clés et types de la collection cible vers la collection cles 
+                    collection_cles.insert_one({'nom_cle':nom_cle, 'type_cle':type(valeur).__name__ })  
                 except Exception as e: 
                     if "duplicate key" in e.details.get("errmsg", str(e)).lower(): # Se déclenche si l'unicité des clés n'est pas respectée. 
                         print(f'La clé {nom_cle} est déja présente')
@@ -28,15 +34,12 @@ def choisir_ou_creer_cles(db, collection):
                         print("Erreur mongo :" , e.details.get("errmsg", str(e)) )
                     
 
-    
-
-    cles = list(collection_cles.find({}, {"_id":0}))
-    print("\nListe des clés existantes\n:")
-    for n, c in enumerate(cles):
-        print(f"[{n}] {c['nom_cle']} (type: {c['type_cle']})")
-        
+#########
+# Inserer une nouvelle clé   
+############     
+     
     while True:
-        choix = input("\nVoulez-vous ajouter une clé (oui ou non)? ").strip().lower()
+        choix = input("\nVoulez-vous ajouter une clé manuellement (oui ou non)? ").strip().lower()
         if choix == "oui":
             nom_cle = input("Nom de la nouvelle clé : ").strip()
             types = ["str", "int", "float", "date"]
@@ -53,10 +56,101 @@ def choisir_ou_creer_cles(db, collection):
             except:
                 print("Réponse invalide.")
         elif choix == "non":
-            return list(collection_cles.find({}, {"_id":0}))
+            break
         else:
             print("Réponse invalide.")
-            return
+            
 
 
+
+
+
+    while True:
+    
+#########
+# Afficher les documents de la collection "cles"    
+############
+
+        cles = list(collection_cles.find({}, {"_id":0}))
+        print("\nListe des clés existantes\n:") 
+        for n, c in enumerate(cles):
+            print(f"[{n}] {c['nom_cle']} (type: {c['type_cle']})")
+
+
+#########
+# Modifier le nom d'une clé   
+#########               
+
+        choix = input(f"\nSouhaitez-vous renommer une clé de la collection {collection_cles.name}?  (oui ou non) ").strip().lower()
+        if choix == "oui":
+                cle_index = int(input(f"\nIndiquer l'index relatif à la clé que vous souhaitez modifier :").strip())
+                ancienne_cle=cles[cle_index]['nom_cle']              
+                nouvelle_cle = input(f"\n Entrer la nouvelle valeur :").strip()
+                try:    
+                    if 0<= cle_index <len(cles):
+                         collection_cles.update_one({"nom_cle": ancienne_cle},{"$set": {"nom_cle": nouvelle_cle}})
+                except:
+                    print("Réponse invalide.")
+        elif choix == "non":
+            None
+        else:
+            print("Réponse invalide.")
+
+#########
+# Modifier le type de la clé   
+#########               
+
+        choix = input(f"\nSouhaitez-vous modifier le type d'une clé de la collection {collection_cles.name}?  (oui ou non) ").strip().lower()
+        if choix == "oui":
+                cle_index = int(input(f"\nIndiquer l'index relatif à la clé que vous souhaitez modifier :").strip())
+                nom_cle=cles[cle_index]['nom_cle']  
+                ancien_type=cles[cle_index]['type_cle'] 
+                
+                print(f'ancien type {ancien_type}')
+                
+                types = ["str", "int", "float", "date"]
+                for n, t in enumerate(types):
+                        print(f"[{n}] {t}")
+                type_index = int(input("Type de la clé (0-3) : ").strip())
+                
+                nouveau_type=types[type_index]
+                try:    
+                    if 0<= cle_index <len(cles):
+                         collection_cles.update_one({"nom_cle": nom_cle},{"$set": {"type_cle": nouveau_type}})
+                except:
+                    print("Réponse invalide.")      
+        elif choix == "non":
+            None
+        else:
+            print("Réponse invalide.")
+        
+ 
+#########
+# Suppression   
+#########               
+
+            
+        choix = input(f"\nSouhaitez-vous supprimer tous les documents de la collection {collection_cles.name}?  (oui ou non) ").strip().lower()
+        if choix == "oui":
+            collection_cles.delete_many({})     
+        elif choix == "non":
+            None
+        else:
+            print("Réponse invalide.")
+
+
+        choix = input(f"\nSouhaitez-vous supprimer un document de la collection {collection_cles.name}?  (oui ou non) ").strip().lower()
+        if choix == "oui":
+                try:
+                cle_index = int(input(f"\nIndiquer l'index relatif à la clé que vous souhaitez modifier :").strip())
+                nom_cle=cles[cle_index]['nom_cle']  
+                    if 0<= cle_index <len(cles):
+                         collection_cles.delete_one({"nom_cle": nom_cle})
+                except:
+                    print("Réponse invalide.")      
+        elif choix == "non":
+            break
+        else:
+            print("Réponse invalide.")
+            
 
