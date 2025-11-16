efac96443096383b5af29af32e7a29a943ba8387
hyhishem <001data2025@gmail.com>
2025-11-16 01:05:26 +0100

    V1.3 correction des scripts

--- scripts/def_nettoyer_et_migrer_csv.py
+++ scripts/def_nettoyer_et_migrer_csv.py
@@ -41,6 +41,33 @@ def nettoyer_et_migrer_csv(db, collection, csv_file, pas_vider_col):
            else:
              print("Erreur mongo :" , e.details.get("errmsg") )
     
+ 
+#Creation d'index
+
+    if  'index_name_date_room' in collection.index_information():
+        return
+    else:
+        try:
+            collection.create_index([("Name", 1), ("Age", 1), ("Date of Admission", 1), ("Room Number", 1)],unique=True, name="index_name_date_room") # Creation de l'index
+        except Exception as e:
+           if  "not authorized" in  e.details.get("errmsg").lower():      
+                print("Vous n'avez pas les droits pour creer un index.")
+           else:
+                print("Erreur mongo :" , e.details.get("errmsg") )
+
+        
+    if  'index_name' in collection.index_information():
+        return
+    else:
+        try:
+            collection.create_index([("Name", 1)],name="index_name") # Creation de l'index
+        except Exception as e:
+           if  "not authorized" in  e.details.get("errmsg").lower():      
+                print("Vous n'avez pas les droits pour creer un index.")
+           else:
+                print("Erreur mongo :" , e.details.get("errmsg") )
+       
+       
     documents = df.to_dict("records")  # conversion de df en dictionnaire 
     
     if documents:
@@ -56,20 +83,3 @@ def nettoyer_et_migrer_csv(db, collection, csv_file, pas_vider_col):
     else:
         print("Aucun document valide.")
     
-    
-    if  'index_name_date_room' in collection.index_information():
-        return
-    else:
-        try:
-            collection.create_index([("Name", 1), ("Age", 1), ("Date of Admission", 1), ("Room Number", 1)],unique=True, name="index_name_date_room") # Creation de l'index
-        except Exception as e:
-           if  "not authorized" in  e.details.get("errmsg").lower():      
-                print("Vous n'avez pas les droits pour creer un index.")
-           else:
-                print("Erreur mongo :" , e.details.get("errmsg") )
-
-        
-
-       
-       
-        
